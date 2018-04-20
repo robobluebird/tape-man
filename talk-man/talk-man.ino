3d913a3f4baa0f5c4eb8f2915b4c748d768a149c
Zachary Schroeder <schroza@gmail.com>
2018-04-20 12:46:58 -0400

    some index fixes (for restarts)

--- talk-man/talk-man.ino
+++ talk-man/talk-man.ino
@@ -9,18 +9,18 @@ WiFiClient client;
 WebSocketClient webSocketClient;
 
 char server[] = "tape-man.herokuapp.com";
-uint8_t bytes[1000];
+uint8_t bytes[250];
 int byteIndex = 0;
 
 char wsPath[] = "/";
 char* pathPtr = wsPath;
 char* hostPtr = server;
 
-// const char* ssid = "WE WORK";
-// const char* password = "P@ssw0rd";
+ const char* ssid = "WE WORK";
+ const char* password = "P@ssw0rd";
 
-const char* ssid = "Fios-HEUK7";
-const char* password = "ton4660bug682deck";
+//const char* ssid = "Fios-HEUK7";
+//const char* password = "ton4660bug682deck";
 
 void setup() {
   webSocketClient.path = pathPtr;
@@ -94,7 +94,7 @@ void loop() {
 
   bytes[byteIndex] = analogValue;
 
-  if (byteIndex >= 999) {
+  if (byteIndex >= 249) {
     if (client.connected()) {
       webSocketClient.sendData(bytes, sizeof(bytes));
     } else {
