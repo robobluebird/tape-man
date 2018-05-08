e068cf90fbbadc80920051d1524490301daefdec
Zachary Schroeder <schroza@gmail.com>
2018-05-08 09:54:08 -0400

    omg wifi

--- asdfasdf/asdfasdf.ino
+++ asdfasdf/asdfasdf.ino
@@ -3,11 +3,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Encoder.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-String networks[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+String networks[50];
 int selectedNetworkIndex = 0;
 int selectedCharacterIndex = 0;
 String ssid = "";
@@ -27,22 +29,85 @@ const int encoderClick = 13;
 void setup() {
   Serial.begin(115200);
 
+  EEPROM.begin(512);
+
   pinMode(encoderClick, INPUT);
   pinMode(5, OUTPUT);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  delay(100);
+  
   display.clearDisplay();
   display.setTextWrap(false);
 
-  selectingPassword = true;
-  printPasswordSelection();
+  resetDisplay();
+
+  display.println("hello.");
+  display.println("Scanning networks...");
+
+  display.display();
+
+  delay(2000);
+
+  scanNetworks();
+  
+//  selectingPassword = true;
+//  printPasswordSelection();
 }
 
-void printNetworks() {
+void scanNetworks() {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  
+  int n = WiFi.scanNetworks();
+  
+  if (n == 0) {
+    resetDisplay();
+    display.println("No networks found.");
+  } else {
+    int len = (n > 50) ? 50 : n;
+    
+    for (int i = 0; i < len; i++) {
+      networks[i] = WiFi.SSID(i);
+    }
+
+    selectingNetwork = true;
+    showNetworks();
+  }
+}
+
+bool savedNetwork() {
+
+}
+
+bool readNetwork() {
+
+}
+
+bool readPassword() {
+
+}
+
+bool writeSSID() {
+
+}
+
+bool writePassword() {
+
+}
+
+void resetDisplay() {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
+}
+
+void showNetworks() {
+  resetDisplay();
+  
   display.println("Choose a network");
 
   for (int i = selectedNetworkIndex; i < selectedNetworkIndex + 3; i++) {
@@ -73,7 +138,7 @@ void shiftUp(int amount = 1) {
     selectedNetworkIndex += amount;
   }
 
-  printNetworks();
+  showNetworks();
 }
 
 void shiftDown(int amount = 1) {
@@ -83,7 +148,7 @@ void shiftDown(int amount = 1) {
     selectedNetworkIndex -= amount;
   }
 
-  printNetworks();
+  showNetworks();
 }
 
 void printPasswordSelection() {
@@ -164,7 +229,7 @@ void loop() {
       if (newPosition > oldPosition) {
         shiftUp(pwdAccel);
       } else {
-        shiftDown();
+        shiftDown(pwdAccel);
       }
 
       oldPosition = newPosition;
@@ -185,7 +250,7 @@ void loop() {
         if (pwdAccel < 5)
           pwdAccel += 1;
       }
-      
+
       lastScrollTime = millis();
     } else {
       if (millis() - lastScrollTime > 100)
